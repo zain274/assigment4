21643b04e4108de6757693153ae4eeff947116e4
zain raza <zrabbas110@gmail.com>
2022-11-05 12:09:06 -0700

    work complete

---
+++ main.dart
@@ -0,0 +1,101 @@
+import 'dart:io';
+
+
+
+
+// question number 8
+ void main(List<String> args) {
+//   print("Enter first number");
+//   int num1=int.parse(stdin.readLineSync()!) ;
+  
+//    print("Enter second number");
+//    int num2=int.parse( stdin.readLineSync()!);
+//       print("Enter second number");
+// String message ='''
+// Select Option
+// 1.Add
+// 2.Sub
+// 3.mul
+// 4.Div
+// 5.Mod
+// Your Choice ?
+// ''';
+//    stdout.write(message);
+//     int userchoice =int.parse(stdin.readLineSync()!);
+//     if(userchoice == 1)
+//     {
+//       print('Sum = ${num1} + ${num2} = ${num1+num2}');
+//     }
+//     else if(userchoice == 2)
+//     {
+//       print('Sub =  ${num1} - ${num2} =${num1-num2}');
+//     }
+//     else if(userchoice == 3)
+//     {
+//        print('mul = ${num1} * ${num2} = ${num1*num2}');
+//     }
+//     else if(userchoice ==4)
+//     {
+//         print('div = ${num1} / ${num2} =  ${num1/num2}');
+//     }
+//     else if(userchoice ==5)
+//     {
+//               print('Mod = ${num1} % ${num2} =  ${num1%num2}');
+
+//     }
+//     else{
+//       print('Invalid Choice');
+//     }
+    
+    
+// question number 9
+
+// print('Enter characher:');
+// String ? ch= stdin.readLineSync();
+// ch =ch?.toLowerCase();
+// if((ch=='a') || (ch=='e' ) || (ch=='i') || (ch=='o') || (ch=='u'))
+// {
+// print('true');
+// }
+// else
+// {
+//   print(' false');
+// }
+
+
+
+
+
+// question 3
+// int num1,i;
+// stdout.write("Enter number =");
+// num1=int.parse(stdin.readLineSync()!);
+// for(i=7; i<15; i++)
+// {
+//   print('${num1} X ${i} =${num1*i}');
+// }
+
+//question 5
+
+// for(int num2=1; num2<100; num2++)
+// {
+//   if(num2%5==0)
+//   {
+//     print(num2);
+//   }
+// }
+
+
+//question 10
+
+// var fruits=["apple","banana","mango","orange","strawberry"];
+// for(int i=1; i<2; i++)
+// {
+//   print(fruits);
+// }
+
+
+
+
+
+}
